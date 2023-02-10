773810ae07810b317d33b556f784919196052883
Aharito <harand@mail.ru>
2023-02-10 16:06:10 +0700

    Update plugin.compare.php

--- assets/plugins/Compare/plugin.compare.php
+++ assets/plugins/Compare/plugin.compare.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * ComparePlugin 
+ * Сравнение товаров
+ * 
+ * @category    plugin
+ * @version     1.0
+ * @author      Aharito (https://aharito.ru)
+ * @internal    @properties &maxCount=Max products;text;5 &tvList=DocLister tvList;text;new,hit,nalich,vid,nazv,tol,dlina,shir,color,material,tekstura,plotn,razm,q_complect,price &sortBy=sortBy;text;menuindex &sortOrder=sortOrder activity;text;DESC;No &prepare=DocLister prepare;text;Tovar_picture,Tovar_labels,Tovar_data
+ * @internal    @events OnPageNotFound
+ * @internal    @modx_category Compare
+ * @internal    @disabled 0
+**/
+
 $maxCount = 5;
 
 if( isset($_GET['q']) && $_GET['q'] == 'compare-list-render') {
